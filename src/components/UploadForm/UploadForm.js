8ba25cba70c4d93067dd3dbec1d0b274400a881e
hnslyswhy <huanyuw01@gmail.com>
2021-12-01 10:12:01 -0800

    fix upload form/input & add tablet style to header

--- src/components/UploadForm/UploadForm.js
+++ src/components/UploadForm/UploadForm.js
@@ -3,11 +3,11 @@ import "./UploadForm.scss";
 
 function UploadForm() {
   return (
-    <div className="upload">
-      {/*  <input className="upload-input" type="file" name="file" /> */}
+    <label className="upload">
+      <input className="upload-input" type="file" name="file" />
       <img className="upload-icon" src={uploadIcon} alt="upload icon" />
       <span className="upload-text">UPLOAD</span>
-    </div>
+    </label>
   );
 }
 
