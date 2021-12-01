2749717aa13199be1f5e4f78a301b0e5d39c2433
hnslyswhy <huanyuw01@gmail.com>
2021-11-30 21:45:40 -0800

    fix data folder files from js to json

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,12 @@
 import MainVideo from "../MainVideo/MainVideo";
 import VideoList from "../VideoList/VideoList";
-import videoDetails from "../../data/videoDetails";
+import data from "../../data/video-details.json";
 
 function Main() {
   return (
     <main className="main">
-      <MainVideo className="main-video" video={videoDetails[0]} />
-      <VideoList className="main-videoList" videos={videoDetails.slice(1)} />
+      <MainVideo className="main-video" video={data[0]} />
+      <VideoList className="main-videoList" videos={data.slice(1)} />
     </main>
   );
 }
