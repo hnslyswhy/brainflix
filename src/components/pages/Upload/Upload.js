011ea27e383547ea8038bb835c33ee2395f55bf3
hnslyswhy <huanyuw01@gmail.com>
2021-12-07 06:12:41 -0800

    fix upload page style

--- src/components/pages/Upload/Upload.js
+++ src/components/pages/Upload/Upload.js
@@ -6,28 +6,33 @@ const Upload = () => {
     <main className="upload">
       <h1 className="upload-headline">Upload Video</h1>
       <form className="upload-form">
-        <label htmlFor="file" className="upload-form-file">
-          VIDEO THUMBNAIL
-          <input id="file" type="file" name="file"></input>
-        </label>
-        <div className="upload-form-entries">
-          <div className="upload-form-section">
-            <label htmlFor="title">TITLE YOUR VIDEO</label>
-            <input
-              type="text"
-              name="title"
-              id="title"
-              placeholder="Add title to your video"
-            ></input>
+        <div className="upload-form-box">
+          <div className="upload-form-video">
+            <label htmlFor="file">VIDEO THUMBNAIL</label>
+            <div className="upload-form-file">
+              <input id="file" type="file" name="file"></input>
+            </div>
           </div>
-          <div className="upload-form-section">
-            <label htmlFor="description">ADD A VIDEO DESCRIPTION</label>
-            <textarea
-              type="text"
-              name="description"
-              id="description"
-              placeholder="Add a description to your video"
-            ></textarea>
+
+          <div className="upload-form-entries">
+            <div className="upload-form-section">
+              <label htmlFor="title">TITLE YOUR VIDEO</label>
+              <input
+                type="text"
+                name="title"
+                id="title"
+                placeholder="Add title to your video"
+              ></input>
+            </div>
+            <div className="upload-form-section">
+              <label htmlFor="description">ADD A VIDEO DESCRIPTION</label>
+              <textarea
+                type="text"
+                name="description"
+                id="description"
+                placeholder="Add a description to your video"
+              ></textarea>
+            </div>
           </div>
         </div>
         <div className="upload-form-buttons">
